242fa84119d42739cb6927c359964eb9db7270a3
luiznetojc <luiznetojc@gmail.com>
2021-08-05 14:23:27 -0300

    range data ajustado no card

--- src/app/views/pedidoslist/pedidoslist.component.ts
+++ src/app/views/pedidoslist/pedidoslist.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ResponsePedido } from 'src/app/modelos/response-pedido';
 import { PedidosService } from 'src/app/services/pedidos.service';
-import {formatDate } from '@angular/common';
+import { formatDate } from '@angular/common';
 import { saveAs } from 'file-saver';
 import { ProdutosService } from 'src/app/services/produtos.service';
 @Component({
@@ -12,81 +12,70 @@ import { ProdutosService } from 'src/app/services/produtos.service';
 })
 export class PedidoslistComponent implements OnInit {
 
-  constructor(private pedidosSvc: PedidosService,private ProdutosSvc: ProdutosService) { }
+  constructor(private pedidosSvc: PedidosService, private ProdutosSvc: ProdutosService) { }
   allPedidos!: Observable<any>;
   allProdutos!: Observable<any>;
   pedidosRangeData!: Observable<any>;
   id = 1;
   ngOnInit(): void {
 
-   this.searchPedido();
+    this.searchPedido();
 
   }
-  reloadPedido()
-  {
+  reloadPedido() {
     this.searchPedido();
   }
-  cancelarNota(id:string)
-  {
- this.pedidosSvc.cancelarNota(id);
+  cancelarNota(id: string) {
+    this.pedidosSvc.cancelarNota(id);
   }
-  begin_data =  "";
+  begin_data = "";
   end_data = "";
-  getAllXML()
-  {
-    this.pedidosRangeData = this.pedidosSvc.getPedidosbyRangeData(this.begin_data,this.end_data);
-    this.pedidosRangeData.subscribe( pedidos =>{
+  getAllXML() {
+    this.pedidosRangeData = this.pedidosSvc.getPedidosbyRangeData(this.begin_data, this.end_data);
+    this.pedidosRangeData.subscribe(pedidos => {
       for (let pedido of pedidos) {
         var res = this.pedidosSvc.getPdfReport(pedido["xml"])
-        res.subscribe( res => {
+        res.subscribe(res => {
           saveAs(res);
-          })
+        })
       }
     })
-  
+
   }
-  getPDF(url: string)
-  {
+  getPDF(url: string) {
     var res = this.pedidosSvc.getPdfReport(url);
-    res.subscribe( res => {
-    const fileURL = URL.createObjectURL(res);
-    window.open(fileURL, '_blank');
+    res.subscribe(res => {
+      const fileURL = URL.createObjectURL(res);
+      window.open(fileURL, '_blank');
     })
   }
-  getXML(url: string)
-  {
+  getXML(url: string) {
     var res = this.pedidosSvc.getXmlReport(url);
-    res.subscribe( res => {
-    const fileURL = URL.createObjectURL(res);
-    window.open(fileURL, '_blank');
+    res.subscribe(res => {
+      const fileURL = URL.createObjectURL(res);
+      window.open(fileURL, '_blank');
     })
   }
-  data = new Date(); 
+  data = new Date();
   dataString = "";
-  formatDate(data: string)
-  {
+  formatDate(data: string) {
     data = formatDate(this.data, 'dd-MM-yyyy hh:mm:ss a', 'en-US', '+0530');
     return data;
   }
-  formatDatePedidos()
-  {
-    this.allPedidos.forEach(data =>
-      {
-        data.data_pedido = this.formatDate(data.data_pedido);
-      } )
+  formatDatePedidos() {
+    this.allPedidos.forEach(data => {
+      data.data_pedido = this.formatDate(data.data_pedido);
+    })
   }
-  searchProdutobyId(id: number)
-  {
+  searchProdutobyId(id: number) {
     this.allProdutos = this.ProdutosSvc.getProdutosById(id);
   }
-  searchPedido()
-  {
+  searchPedido() {
     this.dataString = this.formatDate(this.dataString);
-    this.allPedidos = this.pedidosSvc.getPedidosbyData(this.dataString.substring(0,10));
+    this.allPedidos = this.pedidosSvc.getPedidosbyData(this.dataString.substring(0, 10));
     this.formatDatePedidos();
   }
-  checkNfc(id: string)
-  {
+  checkNfc(id: string) {
     this.pedidosSvc.checkNfc(id)
   }
 }
